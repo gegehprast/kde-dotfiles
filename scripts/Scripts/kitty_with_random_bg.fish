2bfbe8b23fa27705f1803b4e7b1b7e384eaf3c76
gegehprast <36652820+gegehprast@users.noreply.github.com>
2025-05-23 02:43:09 +0700

    Add comments to clarify the purpose of the random wallpaper script

--- scripts/Scripts/kitty_with_random_bg.fish
+++ scripts/Scripts/kitty_with_random_bg.fish
@@ -1,3 +1,6 @@
 #!/usr/bin/env fish
 
+# This script launch Kitty terminal with a random wallpaper from the ~/Pictures/wallpapers directory.
+# Assign this script to a keyboard shortcut.
+
 kitty --override background_image_layout=cscaled --override background_image=(ls -1 --icons=never --absolute --colour=never ~/Pictures/wallpapers/**/*.jpg | sort --random-sort | head -1)
