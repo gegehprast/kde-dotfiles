56e40eda0b864ecfb6a32fe4e937bd31c8534184
gegehprast <36652820+gegehprast@users.noreply.github.com>
2025-05-23 20:00:49 +0700

    Fix wallpaper directory path in random wallpaper script

--- scripts/Scripts/kitty_with_random_bg.fish
+++ scripts/Scripts/kitty_with_random_bg.fish
@@ -3,4 +3,4 @@
 # This script launch Kitty terminal with a random wallpaper from the ~/Pictures/wallpapers directory.
 # Assign this script to a keyboard shortcut.
 
-kitty --override background_image_layout=cscaled --override background_image=(ls -1 --icons=never --absolute --colour=never ~/Pictures/wallpapers/**/*.jpg | sort --random-sort | head -1)
+kitty --override background_image_layout=cscaled --override background_image=(ls -1 --icons=never --absolute --colour=never ~/Pictures/KittyWallpapers/**/*.jpg | sort --random-sort | head -1)
